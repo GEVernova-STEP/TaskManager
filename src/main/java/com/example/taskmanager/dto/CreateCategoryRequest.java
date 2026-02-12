b59150ade986b47c82301196c849b56da926dfbc
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-13 00:42:34 +0530

    [Ashutosh] feat: added create category request dto

---
+++ src/main/java/com/example/taskmanager/dto/CreateCategoryRequest.java
@@ -0,0 +1,18 @@
+package com.example.taskmanager.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DTO for creating category.
+ */
+@Getter
+@Setter
+public class CreateCategoryRequest {
+
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    private String description;
+}
